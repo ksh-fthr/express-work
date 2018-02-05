3d3102041f13926d79386818e8144eb0bfa5752a
ksh-fthr <ksh-fthr@ksh-fthrnoMacBook.local>
2018-02-05 23:33:39 +0900

    GETメソッドのコメント修正

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/**
+ * HTTP の GET メソッドを待ち受けてステータスコードと文字列を返すだけの単純なもの
+ * レスポンスは下記の実装の通り
+ * {
+ *   status: 200,
+ *   response: 'Expressから返却した'
+ * }
+ * といった JSON が返却される
+ */
 router.get('/get', function(req, res, next) {
   res.json({status: 200, response: 'Expressから返却した'});
 });
